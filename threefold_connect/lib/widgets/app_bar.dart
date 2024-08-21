b38b255d008b21939280e614b500aa184b33b3ea
Basem1166 <basem_elgalfy@yahoo.com>
2024-08-21 14:41:13 +0300

    feat: app bar

---
+++ threefold_connect/lib/widgets/app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const MyAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
